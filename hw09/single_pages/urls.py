438fea97b435f7dd2e633f79fc31182a2676b0e0
Gayeon Woo <152853485+gayeonwoo@users.noreply.github.com>
2024-10-26 21:46:42 +0900

    Add files via upload

---
+++ hw09/single_pages/urls.py
@@ -0,0 +1,12 @@
+#표지판 파일
+
+from django.urls import path,include
+from . import views
+
+app_name = 'single_pages'
+
+urlpatterns = [
+    path('gypage/', views.gypage, name='gypage'),
+    path('kmpage/', views.kmpage, name='kmpage'),
+    path('', views.index, name='index'),
+]
