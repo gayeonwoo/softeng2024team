2eecd8caf815fe5c7b7f5115bfd04d8bcd73ae4f
kyungmun <153519952+SongKM2@users.noreply.github.com>
2024-11-02 13:47:24 +0900

    Add files via upload

---
+++ hw10_django/single_pages/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'single_pages'
+
+urlpatterns = [
+    path("kmpage/", views.km, name="km"),
+    path("", views.landing_page, name="landing"),
+    path("gyPage/", views.gy, name="gy"),
+    path("blog/", views.blog, name="blog"),
+]
