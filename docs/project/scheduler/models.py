8b67512e8d54099788e11601282f1539a300ef00
Gayeon Woo <152853485+gayeonwoo@users.noreply.github.com>
2024-12-23 06:10:22 +0000

    a

---
+++ docs/project/scheduler/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Schedule(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
--- docs/send_message.js
+++
@@ -1,3 +0,0 @@
-function message(){
-    alert("Stay happy!")
-}
